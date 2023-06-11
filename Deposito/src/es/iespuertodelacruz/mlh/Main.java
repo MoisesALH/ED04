7b9a9653e2ea8c02066ce85d640885fecaa105a2
moisesALH <moiluher@gmail.com>
2023-06-11 17:41:08 +0100

    refactor(main): Cambiado nombre de variable miCuenta a cuenta1. Por otro lado, implementado las funciones sacarDinero e ingresarDinero

--- Deposito/src/es/iespuertodelacruz/mlh/Main.java
+++ Deposito/src/es/iespuertodelacruz/mlh/Main.java
@@ -3,23 +3,42 @@ package es.iespuertodelacruz.mlh;
 import es.iespuertodelacruz.mlh.Entity.CCuenta;
 
 public class Main {
+    
+    public static String sacarDinero(CCuenta cuenta, double saldo) throws Exception{
+        String respuesta = null;        
+        try {
+            cuenta.retirar(2300);
+            respuesta = saldo + "€ se han retirado de la cuenta: " + cuenta.getCuenta();
+        } catch (Exception e) {
+            e.printStackTrace();            
+        }        
+        return respuesta;
+    }
+    
+    public static String ingresarDinero(CCuenta cuenta, double cantidad) throws Exception{
+        String respuesta = null;
+        try {
+            cuenta.ingresar(695);
+            respuesta = cantidad + "€ se han ingresados en la cuenta: " + cuenta.getCuenta();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return respuesta;
+    }
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
-        double saldoActual;
-
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        CCuenta cuenta1;
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);        
+        System.out.println("El saldo actual es" + cuenta1.estado() );
 
         try {
-            miCuenta.retirar(2300);
+            System.out.println(sacarDinero(cuenta1, 2300));
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
-            System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            System.out.println(ingresarDinero(cuenta1, 695));
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
